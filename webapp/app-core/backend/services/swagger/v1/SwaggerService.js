75ab7d1a544ec436645046b1b47e7740efa91848
Sven Gerhardt <sven.gerhardt@trilobyte-se.de>
2020-10-28 15:06:04 +0100

    swagger ui - webapp core endpoints

---
+++ webapp/app-core/backend/services/swagger/v1/SwaggerService.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require(global.GLOBAL_ROOT + '/swagger.json');
+const AbstractService = require(global.GLOBAL_BACKEND_ROOT + '/services/AbstractService');
+
+class SwaggerService extends AbstractService {
+
+    constructor(serviceName, serviceConfig, app, database) {
+        super(serviceName, serviceConfig, app, database);
+        this.registerRequestHandler();
+    }
+
+    registerRequestHandler() {
+        this.getRouter().use('/', swaggerUi.serve);
+        this.getRouter().get('/', swaggerUi.setup(swaggerDocument, {
+            customCss: '.swagger-ui .topbar { display: none }',
+        }));
+    }
+}
+
+module.exports = SwaggerService;
