59d6f03c9ed8dc3ec3dbb2d695d8bfb4c564214c
Sven Gerhardt <sven.gerhardt@trilobyte-se.de>
2020-11-04 11:29:34 +0100

    check if value is defined or not

--- webapp/app-core/src/utils/ValidationRules.js
+++ webapp/app-core/src/utils/ValidationRules.js
@@ -3,12 +3,12 @@ const validationRules = {
     required: value => !!value || 'Required',
 
     email: value => {
-        if(!value.trim()) return true;
+        if(!value || !value.trim()) return true;
         return /.+@.+/.test(value) || 'E-mail must be valid'
     },
 
     password: value => {
-        if(!value.trim()) return true;
+        if(!value || !value.trim()) return true;
         const text = 'The password must contain at least: 1 uppercase letter (A-Z), 1 lowercase letter (a-z), 1 number (0-9), and one special character (E.g. , . _ & ? etc)';
         const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\u0020-\u002f\u003a-\u0040\u005b-\u0060\u007b-\u007e\u00A7])(?=.{8,})/;
         return pattern.test(value) || text
