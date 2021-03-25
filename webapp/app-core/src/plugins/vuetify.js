8129ae479b2277374ae7e1502267c7e5f5f60b22
Sven Gerhardt <sven.gerhardt@trilobyte-se.de>
2021-03-25 13:21:27 +0100

    renamed app color process envs

--- webapp/app-core/src/plugins/vuetify.js
+++ webapp/app-core/src/plugins/vuetify.js
@@ -18,10 +18,10 @@ const opts = {
   theme: {
     themes: {
       light: {
-        primary: process.env.VUE_APP_COLOR_PRIMARY || '#000000',
-        secondary: process.env.VUE_APP_COLOR_SECONDARY || '#000000',
-        background: process.env.VUE_APP_COLOR_BACKGROUND || '#f9f9f9',
-        navigationDrawer: process.env.VUE_APP_COLOR_NAVIGATION_DRAWER || '#ffffff'
+        primary: process.env.VUE_APP_PRIMARY_COLOR || '#000000',
+        secondary: process.env.VUE_APP_SECONDARY_COLOR || '#000000',
+        background: process.env.VUE_APP_BACKGROUND_COLOR || '#f9f9f9',
+        navigationDrawer: process.env.VUE_APP_NAVIGATION_DRAWER_COLOR || '#ffffff'
       }
     }
   },
