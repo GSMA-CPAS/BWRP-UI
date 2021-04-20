e4a62777d567da9ed0db483f080060d84f0a5f85
Timothy Antonovics <tantonovics@trilobyte-se.de>
2021-04-20 12:30:14 +0200

    fixes issue where data props was forgotten in its model template

--- webapp/app-roaming/src/components/dialogs/contract-tabs/models/BalancedUnbalancedModelTemplate.vue
+++ webapp/app-roaming/src/components/dialogs/contract-tabs/models/BalancedUnbalancedModelTemplate.vue
@@ -1,36 +1,48 @@
 <template>
   <v-col class="striped-column-container">
     <v-row>
-      <v-col class="striped-column"><b>{{ modelType }}</b></v-col>
+      <v-col class="striped-column">
+        <b>{{ modelType }}</b>
+      </v-col>
       <v-col class="striped-column">{{ data.unit }}</v-col>
       <v-col class="striped-column"></v-col>
       <v-col class="striped-column"></v-col>
       <v-col class="striped-column"></v-col>
-      <v-col class="striped-column"><v-icon v-if="inCommitment" color="primary">
-        mdi-checkbox-marked-outline
-      </v-icon></v-col>
+      <v-col class="striped-column">
+        <v-icon v-if="inCommitment" color="primary">
+          mdi-checkbox-marked-outline
+        </v-icon>
+      </v-col>
     </v-row>
     <v-divider />
     <v-row>
       <v-col class="striped-column">Balanced</v-col>
       <v-col class="striped-column"></v-col>
       <v-col class="striped-column">0 ➜ Unlimited </v-col>
-      <v-col class="striped-column">{{ Number(data.ratingPlan.balancedRate.fixedPrice || 0).toLocaleString() }}</v-col>
-      <v-col class="striped-column">{{ Number(data.ratingPlan.balancedRate.linearPrice || 0).toLocaleString() }}</v-col>
+      <v-col class="striped-column">{{
+        Number(data.ratingPlan.balancedRate.fixedPrice || 0).toLocaleString()
+      }}</v-col>
+      <v-col class="striped-column">{{
+        Number(data.ratingPlan.balancedRate.linearPrice || 0).toLocaleString()
+      }}</v-col>
       <v-col class="striped-column"></v-col>
-      <!--<v-col>
+      <!-- <v-col>
         <v-icon v-if="revenueCommitment" color="primary"
           >mdi-checkbox-marked-outline</v-icon
         >
-      </v-col>-->
+      </v-col> -->
     </v-row>
     <v-divider />
     <v-row>
       <v-col class="striped-column">Unbalanced</v-col>
       <v-col class="striped-column"></v-col>
       <v-col class="striped-column">0 ➜ Unlimited </v-col>
-      <v-col class="striped-column">{{ Number(data.ratingPlan.unbalancedRate.fixedPrice || 0).toLocaleString() }}</v-col>
-      <v-col class="striped-column">{{ Number(data.ratingPlan.unbalancedRate.linearPrice || 0).toLocaleString() }}</v-col>
+      <v-col class="striped-column">{{
+        Number(data.ratingPlan.unbalancedRate.fixedPrice || 0).toLocaleString()
+      }}</v-col>
+      <v-col class="striped-column">{{
+        Number(data.ratingPlan.unbalancedRate.linearPrice || 0).toLocaleString()
+      }}</v-col>
       <v-col class="striped-column"></v-col>
       <!--<v-col>
         <v-icon v-if="revenueCommitment" color="primary"
@@ -47,6 +59,6 @@ export default {
   name: 'balanced-unbalanced-model-template',
   model: 'Balanced Unbalanced Model Template',
   mixins: [discountModelsMixin],
-  props: ['modelType', 'inCommitment']
+  props: ['modelType', 'inCommitment', 'data'],
 };
 </script>
