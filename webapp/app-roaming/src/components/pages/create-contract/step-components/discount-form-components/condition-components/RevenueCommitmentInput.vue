7c3de120ee1e2578949e60fa08a60ed8e3216ef5
Timothy Antonovics <tantonovics@trilobyte-se.de>
2021-05-04 19:10:12 +0200

    fixes issue where value wasn't properly displayed in revenue-commitment-input

--- webapp/app-roaming/src/components/pages/create-contract/step-components/discount-form-components/condition-components/RevenueCommitmentInput.vue
+++ webapp/app-roaming/src/components/pages/create-contract/step-components/discount-form-components/condition-components/RevenueCommitmentInput.vue
@@ -1,15 +1,15 @@
 <template>
   <v-row>
-      <v-col>
-        <v-currency-field v-model="commitmentValue" label="Value"/>
-      </v-col>
-      <v-col>
-        <currency-selector v-model="currency" label="Currency"/>
-      </v-col>
-      <v-col>
-        <v-checkbox label="Including Taxes?" v-model="includingTaxes"/>
-      </v-col>
-    </v-row>
+    <v-col>
+      <v-currency-field v-model="commitmentValue" label="Value" />
+    </v-col>
+    <v-col>
+      <currency-selector v-model="currency" label="Currency" />
+    </v-col>
+    <v-col>
+      <v-checkbox label="Including Taxes?" v-model="includingTaxes" />
+    </v-col>
+  </v-row>
 </template>
 <script>
 import CurrencySelector from '@/components/global-components/CurrencySelector';
@@ -41,9 +41,13 @@ export default {
     },
     currency: {
       handler() {
-        if ( this.$data.currency !== this.defaultCurrency ) {
-          if ( !confirm('The currency for the revenue commitment differs from the contract currency. Are you sure?') ) {
-            Vue.nextTick(() => this.$data.currency = this.defaultCurrency);
+        if (this.$data.currency !== this.defaultCurrency) {
+          if (
+            !confirm(
+              'The currency for the revenue commitment differs from the contract currency. Are you sure?',
+            )
+          ) {
+            Vue.nextTick(() => (this.$data.currency = this.defaultCurrency));
           }
         }
         this.$emit('input', this.$data);
@@ -51,10 +55,11 @@ export default {
       deep: true,
     },
   },
-  beforeMount() {
-    if ( this.value ) {
+  mounted() {
+    console.log(this.value);
+    if (this.value) {
       this.includingTaxes = this.value.includingTaxes;
-      this.commitmentValue = this.value.commitmentValue;
+      this.commitmentValue = this.value.value;
       this.currency = this.value.currency;
     } else {
       this.currency = this.defaultCurrency;
