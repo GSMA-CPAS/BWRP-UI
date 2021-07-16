55e6cc33c15dab76b75a7272aa83601108ae3271
Zsolt Schäfer <zsolt.schaefer@t-systems.com>
2021-07-16 13:43:21 +0200

    Password minimum length changed to 12 characters (according to PSA requirement)

--- webapp/app-core/src/utils/ValidationRules.js
+++ webapp/app-core/src/utils/ValidationRules.js
@@ -10,7 +10,7 @@ const validationRules = {
   password: (value) => {
     if (!value || !value.trim()) return true;
     const text = 'The password must contain at least: 1 uppercase letter (A-Z), 1 lowercase letter (a-z), 1 number (0-9), and one special character (E.g. , . _ & ? etc)';
-    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\u0020-\u002f\u003a-\u0040\u005b-\u0060\u007b-\u007e\u00A7])(?=.{8,})/;
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\u0020-\u002f\u003a-\u0040\u005b-\u0060\u007b-\u007e\u00A7])(?=.{12,})/;
     return pattern.test(value) || text;
   },
 
